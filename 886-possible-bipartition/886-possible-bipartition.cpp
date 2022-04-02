abc24e7e7d0fac17502dbc34fb9f5f0ba69e8151
Shivangi Maurya <79085511+shivangimaurya11@users.noreply.github.com>
2022-04-03 00:46:59 +0530

    Time: 196 ms (87.72%), Space: 71.2 MB (38.37%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    //Time: O(V+E), Space: O(V+E)
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        vector<int> color(n+1, -1);
+        vector<int> graph[n+1];
+        
+        //building the graph
+        for(auto edge : dislikes){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        
+        for(int i=1; i<=n; i++){
+            if(color[i] == -1)
+                if(!dfs(graph, i, color)) return false;
+        }
+        return true;
+    }
+    
+    bool dfs(vector<int> graph[], int vertex, vector<int>& color){
+        if(color[vertex] == -1) color[vertex] = 1;
+        
+        for(auto it : graph[vertex]){
+            if(color[it] == -1){
+                color[it] = 1 - color[vertex];
+                if(!dfs(graph, it, color)) return false;
+            }
+            /*
+            if the vertex and it of same color then surely the graph can't be
+            bipartite
+            */
+            else if(color[it] == color[vertex]) return false;
+        }
+        return true;
+    }
+};
